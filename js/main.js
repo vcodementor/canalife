b1b5aca2a05b982ec9be06da5a252b4319e99a86
vcodementor <vcodementor@gmail.com>
2023-10-31 09:26:24 -0400

    animation added

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
-var swiper = new Swiper('.swiper-container', {
-    // Optional Swiper configuration options
-    slidesPerView: 1, // Number of slides visible in one view
-    spaceBetween: 20, // Space between slides
-    // Add more options as needed
+var swiper = new Swiper('#testonominal-slider', {
+    loop: true,
+    // centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
@@ -11,13 +13,31 @@ var swiper = new Swiper('.swiper-container', {
       el: '.swiper-pagination',
       clickable: true,
     },
+    breakpoints: {
+        640: {
+          slidesPerView: 1,
+          spaceBetween: 10,
+        },
+        768: {
+          slidesPerView: 1,
+          spaceBetween: 10,
+        },
+        1024: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+    }
   });
 
   var swiper = new Swiper('#top-categories-slider', {
-    // Optional Swiper configuration options
-    slidesPerView: 4, // Number of slides visible in one view
-    spaceBetween: 20, // Space between slides
-    // Add more options as needed
+    loop: true,
+    // centeredSlides: true,
+    slidesPerView: 1,
+    spaceBetween: 10,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
@@ -26,5 +46,51 @@ var swiper = new Swiper('.swiper-container', {
       el: '.swiper-pagination',
       clickable: true,
     },
+     breakpoints: {
+        640: {
+          slidesPerView: 1,
+          spaceBetween: 10,
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+        },
+        1024: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+    }
   });
 
+
+  var swiper = new Swiper('#home-hero-slider', {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 10,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    }
+  });
+
+  var swiper = new Swiper(".mySwiper", {
+    effect: "cube",
+    grabCursor: true,
+    cubeEffect: {
+      shadow: true,
+      slideShadows: true,
+      shadowOffset: 20,
+      shadowScale: 0.94,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
