5366eff416b23ef561a93dfb7cd55be5690261e9
vcodementor <vcodementor@gmail.com>
2023-10-30 22:32:30 -0400

    animation added

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var swiper = new Swiper('.swiper-container', {
     // Optional Swiper configuration options
     slidesPerView: 1, // Number of slides visible in one view
-    // spaceBetween: 20, // Space between slides
+    spaceBetween: 20, // Space between slides
     // Add more options as needed
     navigation: {
       nextEl: '.swiper-button-next',
@@ -12,3 +12,19 @@ var swiper = new Swiper('.swiper-container', {
       clickable: true,
     },
   });
+
+  var swiper = new Swiper('#top-categories-slider', {
+    // Optional Swiper configuration options
+    slidesPerView: 4, // Number of slides visible in one view
+    spaceBetween: 20, // Space between slides
+    // Add more options as needed
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+
